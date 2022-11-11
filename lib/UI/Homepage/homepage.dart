a5b3288cdc37e4a266350d70acddb63e1b26f277
DanarGdg <danargading@gmail.com>
2022-11-11 09:02:30 +0700

    feat: [DG] new

--- lib/UI/Homepage/homepage.dart
+++ lib/UI/Homepage/homepage.dart
@@ -1,7 +1,6 @@
 import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'package:vespa/widget/aksesorisList.dart';
 import 'package:vespa/widget/gtsVespasList.dart';
 import 'package:vespa/widget/limitedVespaList.dart';
@@ -38,116 +37,114 @@ class _HomepageState extends State<Homepage> with TickerProviderStateMixin {
         centerTitle: true,
         ),
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.only(top: 20, bottom: 10),
-              child: Text(
-                'Choose Your Vespa',
-                style: TextStyle(
-                    fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),
+      body: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 10),
+            margin: EdgeInsets.only(top: 20, bottom: 10),
+            child: Text(
+              'Choose Your Vespa',
+              style: TextStyle(
+                  fontSize: 18,
+                fontWeight: FontWeight.bold
               ),
             ),
-            Container(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: TabBar(
-                  controller: _tabController,
-                  labelPadding: EdgeInsets.symmetric(horizontal: 5),
-                  isScrollable: true,
-                  labelColor: Colors.white,
-                  unselectedLabelColor: Color.fromRGBO(86,194,159,100),
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  indicator: BubbleTabIndicator(
-                    indicatorHeight: 33,
-                    indicatorColor: Color.fromRGBO(86,194,159, 100),
-                    tabBarIndicatorSize: TabBarIndicatorSize.tab,
-                    indicatorRadius: 10,
-                  ),
-                  tabs: [
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Color.fromRGBO(86,194,159, 100))),
-                      child: Text(
-                        'Limited',
-                        style: TextStyle(fontSize: 14, fontFamily: 'OpenSans'),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Color.fromRGBO(86,194,159, 100))),
-                      child: Text(
-                        'GTS',
-                        style: TextStyle(fontSize: 14, fontFamily: 'OpenSans'),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Color.fromRGBO(86,194,159, 100))),
-                      child: Text(
-                        'Sprint',
-                        style: TextStyle(fontSize: 14, fontFamily: 'OpenSans'),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Color.fromRGBO(86,194,159, 100))),
-                      child: Text(
-                        'Primavera',
-                        style: TextStyle(fontSize: 14, fontFamily: 'OpenSans'),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-              Container(
-                width: double.maxFinite,
-                height: 250,
-                child: TabBarView(
-                  controller: _tabController,
-                  children: [
-                    LimitedVespaList(),
-                    GtsVespaList(),
-                    SprintVespasList(),
-                    PrimaveraVespasList(),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10, left: 10, top: 20, bottom: 20),
-                child: Row(
-                  children: [
-                    Text(
-                      "Featured Accesories",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                    Spacer(),
-                    Text(
-                      "View More",
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                ),
-              ),
-              Container(child: AksesorisList())
-            ],
           ),
+          Container(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                controller: _tabController,
+                labelPadding: EdgeInsets.symmetric(horizontal: 5),
+                isScrollable: true,
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.black,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicator: BubbleTabIndicator(
+                  indicatorHeight: 35,
+                  indicatorColor: Color.fromRGBO(86,194,159, 100),
+                  tabBarIndicatorSize: TabBarIndicatorSize.tab,
+                  indicatorRadius: 10,
+                ),
+                tabs: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.black12)),
+                    child: Text(
+                      'Limited',
+                      style: TextStyle(fontSize: 14, fontFamily: 'OpenSans'),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.black12)),
+                    child: Text(
+                      'GTS',
+                      style: TextStyle(fontSize: 14, fontFamily: 'OpenSans'),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.black12)),
+                    child: Text(
+                      'Sprint',
+                      style: TextStyle(fontSize: 14, fontFamily: 'OpenSans'),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.black12)),
+                    child: Text(
+                      'Primavera',
+                      style: TextStyle(fontSize: 14, fontFamily: 'OpenSans'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+            Container(
+              width: double.maxFinite,
+              height: 250,
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  LimitedVespaList(),
+                  GtsVespaList(),
+                  SprintVespasList(),
+                  PrimaveraVespasList(),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10, top: 20, bottom: 20),
+              child: Row(
+                children: [
+                  Text(
+                    "Featured Accesories",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Spacer(),
+                  Text(
+                    "View More",
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              ),
+            ),
+            Container(child: AksesorisList())
+          ],
         ),
       ),
     );
